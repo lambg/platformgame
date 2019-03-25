e43cf24f4bcb192f4a1ab81894aa75d5fd896f44
thieraufk@wit.edu <thieraufk@wit.edu>
2019-03-25 10:00:36 -0400

    Basic World/WorldSegment implementations

--- src/platformer/world/World.java
+++ src/platformer/world/World.java
@@ -1,22 +1,31 @@
 package platformer.world;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class World {
-    public void updateAround(WorldObj obj) {
-        // todo - get segments around obj and update
-        // todo - make sure multiple updates do not happen for single entity
-//        for (WorldSegment segment : MainClient.SERVER.getSegmentsToUpdate(this)) {
-//            segment.update();
-//        }
+    // how many segments to update on either side of each player
+    private static final int UPDATE_SEGMENTS = 5;
+    // the size of UPDATE_SEGMENT segments
+    private static final double UPDATE_SEGMENT_SIZE = WorldSegment.WORLD_SEGMENT_SIZE * UPDATE_SEGMENTS;
+    private List<WorldSegment> positiveSegments = new ArrayList<>();
+    private List<WorldSegment> negativeSegments = new ArrayList<>();
+
+    public void updateAround(List<? extends WorldObj> objCol) {
+        Set<WorldSegment> segments = new HashSet<>();
+        for (WorldObj object : objCol) {
+            WorldSegment here = getSegmentAt(object.getLocation());
+            segments.addAll(getSegmentsFrom(here.getLeftPosX() - UPDATE_SEGMENT_SIZE, here.getRightPosX() + UPDATE_SEGMENT_SIZE));
+        }
+
+        for (WorldSegment segment : segments)
+            segment.update();
+        WorldSegment.transferObjects(); // move objects that switched segments over the course of this update
     }
 
     // todo - keep track of world segments
     // todo - when players advance, create new world segments
 
-    public <T extends WorldObj> Collection<T> getNearbyObjects(Class<T> cl, int lx, int ux, int ly, int uy) {
+    public <T extends WorldObj> Collection<T> getNearbyObjects(Class<T> cl, double lx, double ux, double ly, double uy) {
         List<T> objects = new ArrayList<>();
         for (WorldSegment segment : getSegmentsFrom(lx, ux)) {
             for (WorldObj obj : segment.getObjects()) {
@@ -28,9 +37,38 @@ public class World {
         return objects;
     }
 
-    public Collection<WorldSegment> getSegmentsFrom(int lw, int ux) {
-        // todo
-        return null;
+    public Collection<WorldSegment> getSegmentsFrom(double lx, double ux) {
+        List<WorldSegment> segmentList = new ArrayList<>();
+        for (double x = lx; x <= ux; x += WorldSegment.WORLD_SEGMENT_SIZE) {
+            segmentList.add(getSegmentAt(x));
+        }
+        return segmentList;
+    }
+
+    public WorldSegment getSegmentAt(double x) {
+        // get segment list
+        List<WorldSegment> segments;
+        if (x < 0) {
+            x = -x;
+            segments = negativeSegments;
+        } else segments = positiveSegments;
+
+        // generate missing segments
+        int segmentIndex = (int) (x % WorldSegment.WORLD_SEGMENT_SIZE);
+        while (segmentIndex >= segments.size()) {
+            segments.add(new WorldSegment(this, segments.size()));
+        }
+
+        return segments.get(segmentIndex);
+    }
+
+    public WorldSegment getSegmentAt(Location location) {
+        return getSegmentAt(location.getX());
+    }
+
+    public double getTerrainHeightAt(double x) {
+        WorldSegment segment = getSegmentAt(x);
+        return segment.getTerrainHeightAtLocalPos(x - segment.getLeftPosX());
     }
 
     // todo - shortcuts for world segment (i.e. getTerrainHeight)
--- src/platformer/world/WorldSegment.java
+++ src/platformer/world/WorldSegment.java
@@ -7,34 +7,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WorldSegment {
+    // amount of segments per block
     public static final int BLOCKS_PER_SEGMENT = 10;
+    // size of each block in segment
     public static final int TERRAIN_BLOCK_SIZE = 50;
+    // size of each segment
     public static final int WORLD_SEGMENT_SIZE = BLOCKS_PER_SEGMENT * TERRAIN_BLOCK_SIZE;
+    private final World owner;
     private List<WorldObj> objects = new ArrayList<>();
     private Block[] terrainBlocks = new Block[BLOCKS_PER_SEGMENT];
     private int terrainSegmentIndex;
 
-    public WorldSegment(int terrainSegmentIndex) {
+    public WorldSegment(World owner, int terrainSegmentIndex) {
+        this.owner = owner;
         this.terrainSegmentIndex = terrainSegmentIndex;
 
-        // todo - create terrain blocks
+        for (int i = 0; i < terrainBlocks.length; i++) {
+            terrainBlocks[i] = new Block(i);
+        }
+    }
+
+    private static final List<Tuple> transferredObject = new ArrayList<>();
+
+    static void transferObjects() { // not the most efficient implementation, but application is basic
+        for (Tuple transferObj : transferredObject) {
+            transferObj.from.objects.remove(transferObj.obj);
+            transferObj.to.objects.add(transferObj.obj);
+        }
+        transferredObject.clear();
     }
 
     public void update() {
         for (WorldObj obj : objects) {
             obj.update();
 
-            // todo - if the obj's new segment is not this segment, send the obj to the new segment
+            WorldSegment currentSeg = owner.getSegmentAt(obj.getLocation());
+            if (currentSeg != this)
+                transferredObject.add(new Tuple(obj, this, currentSeg));
         }
 
         for (Block block : terrainBlocks) {
-            block.rectangle.setX(getLocalOffset(block.id));
+            block.rectangle.setX(getLocalOffset(block.id)); // todo - x should be set to offset relative to screen
         }
-        // todo - update entities in segment
     }
 
     public List<WorldObj> getObjects() {
         return objects;
+    }
+
+    public int getLeftPosX() {
+        return terrainSegmentIndex * WORLD_SEGMENT_SIZE;
+    }
+
+    public int getRightPosX() {
+        return (terrainSegmentIndex + 1) * WORLD_SEGMENT_SIZE - 1;
+    }
+
+    // localPosX is the pos local to this segment; 0 represents the left corner of this segment, not the left corner of the world
+    public double getTerrainHeightAtLocalPos(double localPosX) {
+        return getBlockAtLocalPos(localPosX).height;
+    }
+
+    private Block getBlockAtLocalPos(double localPosX) {
+        int blockIndex = (int) (localPosX % TERRAIN_BLOCK_SIZE);
+        if (blockIndex >= terrainBlocks.length)
+            throw new IllegalArgumentException("Block index \"" + blockIndex + "\" is out of bounds; localPosX should be relative to left corner of segment");
+        return terrainBlocks[blockIndex];
     }
 
     public static int getSegmentAt(double x) {
@@ -53,15 +91,44 @@ public class WorldSegment {
         return index * TERRAIN_BLOCK_SIZE;
     }
 
-    private static class Block {
+    private class Block {
         private Rectangle rectangle;
         private int height;
         private int id;
 
-        public Block(Rectangle rectangle, int height, int id) {
-            this.rectangle = rectangle;
-            this.height = height;
+        public Block(int id) {
             this.id = id;
+
+            // todo - generate proper height
+            this.height = id == 0 ? 0 : getLeftBlock().height + 1;
+            this.rectangle = new Rectangle(getLeftBlockPosX(), 0, TERRAIN_BLOCK_SIZE, height);
+        }
+
+        double getLeftBlockPosX() {
+            return getLeftPosX() + id * TERRAIN_BLOCK_SIZE;
+        }
+
+        double getRightBlockPosX() {
+            return getRightPosX() + id * TERRAIN_BLOCK_SIZE;
+        }
+
+        Block getLeftBlock() {
+            return terrainBlocks[id - 1];
+        }
+
+        Block getRightBlock() {
+            return terrainBlocks[id + 1];
+        }
+    }
+
+    private static class Tuple {
+        final WorldObj obj;
+        final WorldSegment from, to;
+
+        public Tuple(WorldObj obj, WorldSegment from, WorldSegment to) {
+            this.obj = obj;
+            this.from = from;
+            this.to = to;
         }
     }
 
