58619cc0835bc2abd0428788d0e595e42c458fdd
thieraufk@wit.edu <thieraufk@wit.edu>
2019-03-26 10:37:21 -0400

    Implemented Object move packet

--- src/platformer/connection/packets/ObjMovePacket.java
+++ src/platformer/connection/packets/ObjMovePacket.java
@@ -1,9 +1,12 @@
 package platformer.connection.packets;
 
+import platformer.GameUtil;
 import platformer.connection.Communicator;
 import platformer.connection.Packet;
 import platformer.world.Location;
+import platformer.world.WorldObj;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -12,18 +15,29 @@ public class ObjMovePacket extends Packet {
     private int objectId;
     private Location toLocation;
 
-    @Override
-    protected void breakdown(OutputStream out) {
+    public ObjMovePacket(int objectId, Location toLocation) {
+        this.objectId = objectId;
+        this.toLocation = toLocation;
+    }
 
+    // called reflectively
+    public ObjMovePacket() {
     }
 
     @Override
-    protected void buildPacket(InputStream in) {
+    protected void breakdown(OutputStream out) throws IOException {
+        GameUtil.write(out, objectId);
+        GameUtil.write(out, toLocation);
+    }
 
+    @Override
+    protected void buildPacket(InputStream in) throws IOException, ClassNotFoundException {
+        objectId = GameUtil.readInt(in);
+        objectId = GameUtil.read(in);
     }
 
     @Override
     public void applyPacket(Communicator communicator, Socket socket) {
-
+        WorldObj.getObject(objectId).setLocation(toLocation);
     }
 }
