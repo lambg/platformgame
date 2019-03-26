287c679b26f21ac241a0e18e1dd5282f57bfcc36
thieraufk@wit.edu <thieraufk@wit.edu>
2019-03-26 10:41:42 -0400

    Implemented re-spawn packet

--- src/platformer/connection/packets/PlayerRespawnPacket.java
+++ src/platformer/connection/packets/PlayerRespawnPacket.java
@@ -1,9 +1,13 @@
 package platformer.connection.packets;
 
+import platformer.GameUtil;
+import platformer.MainClient;
 import platformer.connection.Communicator;
 import platformer.connection.Packet;
 import platformer.world.Location;
+import platformer.world.WorldObj;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -17,6 +21,10 @@ public class PlayerRespawnPacket extends Packet {
         this.reSpawnLocation = reSpawnLocation;
     }
 
+    // called reflectively
+    public PlayerRespawnPacket() {
+    }
+
     public int getEntityId() {
         return entityId;
     }
@@ -26,17 +34,20 @@ public class PlayerRespawnPacket extends Packet {
     }
 
     @Override
-    protected void breakdown(OutputStream out) {
-
+    protected void breakdown(OutputStream out) throws IOException {
+        GameUtil.write(out, entityId);
+        GameUtil.write(out, reSpawnLocation);
     }
 
     @Override
-    protected void buildPacket(InputStream in) {
-
+    protected void buildPacket(InputStream in) throws IOException, ClassNotFoundException {
+        entityId = GameUtil.readInt(in);
+        reSpawnLocation = GameUtil.read(in);
     }
 
     @Override
     public void applyPacket(Communicator communicator, Socket socket) {
-
+        // todo - make sure this client does not send an update packet (move packet) back to server if the server calls this player to re-spawn at a specific location
+        MainClient.WORLD.addObjectToWorld(WorldObj.getObject(entityId));
     }
 }
