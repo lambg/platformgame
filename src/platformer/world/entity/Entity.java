72a172eb7be28f2f720c7dafb82807b85437423c
lambg <lydonjr@gmail.com>
2019-03-27 10:14:52 -0400

    Updated entity classes

--- src/platformer/world/entity/Entity.java
+++ src/platformer/world/entity/Entity.java
@@ -3,7 +3,8 @@ package platformer.world.entity;
 import platformer.world.WorldObj;
 
 public class Entity extends WorldObj {
+
     public Entity() {
-        super(null, 0); // todo - take Location in Entity
+        super();
     }
 }
--- src/platformer/world/entity/LivingEntity.java
+++ src/platformer/world/entity/LivingEntity.java
@@ -1,4 +1,37 @@
 package platformer.world.entity;
 
 public class LivingEntity extends Entity {
+
+    private int maxHealth;
+    private int currentHealth;
+
+    public LivingEntity(int health) {
+        super();
+        this.maxHealth = health;
+        currentHealth = maxHealth;
+    }
+
+    public int getHealth() {
+        return currentHealth;
+    }
+
+    public void increaseHealth(int value) {
+        currentHealth += value;
+
+    }
+
+    public void increaseHealth() {
+        currentHealth++;
+    }
+
+    public void decreaseHealth(int value) {
+        currentHealth -= value;
+    }
+
+    public void decreaseHealth() {
+        currentHealth--;
+    }
+
+
+
 }
